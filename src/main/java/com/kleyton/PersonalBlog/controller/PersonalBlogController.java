ba3aad2794898ff7ed5a12c4dee0fa5634e7f667
KleytonFMadruga <kleyton.fernandes@dcx.ufpb.br>
2022-03-02 23:36:33 -0300

    renders page details (part 1)

--- src/main/java/com/kleyton/PersonalBlog/controller/PersonalBlogController.java
+++ src/main/java/com/kleyton/PersonalBlog/controller/PersonalBlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,10 +19,18 @@ public class PersonalBlogController {
 	PersonalBlogService personalBlogService;
 	
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
-	public ModelAndView getPosts () {
+	public ModelAndView getPosts() {
 		ModelAndView mv = new ModelAndView("posts"); 
 		List<Post> posts = personalBlogService.findAll();
 		mv.addObject("posts", posts);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails"); 
+		Post post = personalBlogService.findById(id);
+		mv.addObject("post", post);
 		return mv;
 	}
 }
