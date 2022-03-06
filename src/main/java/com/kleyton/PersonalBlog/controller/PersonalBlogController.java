18dc5b5e52030560eca55b20d51f8538bf0b69d2
KleytonFMadruga <kleyton.fernandes@dcx.ufpb.br>
2022-03-06 17:12:42 -0300

    Render forms and submit content from the 'newpost' page

--- src/main/java/com/kleyton/PersonalBlog/controller/PersonalBlogController.java
+++ src/main/java/com/kleyton/PersonalBlog/controller/PersonalBlogController.java
@@ -1,13 +1,18 @@
 package com.kleyton.PersonalBlog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kleyton.PersonalBlog.model.Post;
 import com.kleyton.PersonalBlog.service.PersonalBlogService;
@@ -33,4 +38,20 @@ public class PersonalBlogController {
 		mv.addObject("post", post);
 		return mv;
 	}
+	
+	@RequestMapping(value = "/newPost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newPost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		personalBlogService.save(post);
+		return "redirect:/posts";
+	}
+	
 }
